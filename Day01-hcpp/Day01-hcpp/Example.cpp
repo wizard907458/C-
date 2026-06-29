cf9101587339035fcd1a1f0b8122f8e79ca59f5b
maverick_genius <hz13137763291@163.com>
2026-06-29 18:30:05 +0800

    hcpp

---
+++ Day01-hcpp/Day01-hcpp/Example.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"Example.h"
+
+MyClass::MyClass()
+{
+
+}
+
+void MyClass::myFunction()
+{
+	std::cout << "Hello from MyClass::myFunction!" << std::endl;
+}
---
+++ Day01-hcpp/Day01-hcpp/Example.h
@@ -0,0 +1,13 @@
+//#pragma once;
+
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+class MyClass
+{
+public:
+	MyClass();
+	void myFunction();
+};
+
+#endif
